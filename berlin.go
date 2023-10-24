70d52593bac0af4e487113a17aa72127be7e56fb
puvvadip <puvvadip@amazon.com>
2023-10-24 23:27:19 +0200

    working

--- berlin.go
+++ berlin.go
@@ -53,46 +53,53 @@ func initializeSearch(ctx context.Context) error {
 
 	log.Println("Done.")
 	log.Println("Filling Out Form...")
-	countrySelectorDropdownXPath := `//*[@id="xi-sel-400"]`
-	countries := []*cdp.Node{}
+
+	err := fillForm(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to fill form: %w", err)
+	}
+
+	log.Println("Checking for unfilled form...")
+	formBodyXPath := `//*[@id="xi-fs-19"]`
+	formBodyHTML := ""
 
 	chromedp.Run(ctx,
-		chromedp.WaitVisible(countrySelectorDropdownXPath, chromedp.BySearch),
-		chromedp.Nodes(countrySelectorDropdownXPath, &countries, chromedp.BySearch),
+		chromedp.OuterHTML(formBodyXPath, &formBodyHTML, chromedp.BySearch),
 	)
 
-	for _, c := range countries {
-		for _, child := range c.Children {
-			if len(child.Attributes) < countryNameAttributeIdx {
-				return fmt.Errorf("malformed country dropdown attributes")
-			}
-
-			if strings.Contains(child.Attributes[5], desiredCountry) {
-				err := chromedp.Run(ctx,
-					// this form is super janky idk why but it needs this
-					chromedp.Sleep(2*time.Second),
-					chromedp.SetValue(`//select[@id="xi-sel-400"]`, child.Attributes[3], chromedp.BySearch),
-
-					// this sets it to one person
-					chromedp.Sleep(time.Second),
-					chromedp.SetValue(`//select[@id="xi-sel-422"]`, "1", chromedp.BySearch),
-					// this sets it to no other family
-					chromedp.Sleep(time.Second),
-					chromedp.SetValue(`//select[@id="xi-sel-427"]`, "2", chromedp.BySearch),
-				)
-				if err != nil {
-					fmt.Println(err.Error())
-				}
-			}
+	for strings.Contains(formBodyHTML, "field is required") {
+		log.Println("Form is not properly filled, refilling...")
+		err := fillForm(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to fill form: %w", err)
 		}
 	}
 
 	log.Println("Done.")
 	log.Println("Clicking Extend A Residence Title ...")
 	extendAResidenceTitleXPath := `//*[@id="xi-div-30"]/div[2]/label/p`
+	extendAResidenceTitleHTML := ""
 
 	chromedp.Run(ctx,
 		chromedp.WaitVisible(extendAResidenceTitleXPath, chromedp.BySearch),
+		chromedp.OuterHTML(extendAResidenceTitleXPath, &extendAResidenceTitleHTML, chromedp.BySearch),
+	)
+
+	for !strings.Contains(extendAResidenceTitleHTML, "Extend a residence title") {
+		fmt.Println("Selected Block Doesn't Contain Extend A Residence Title. Toggling Family Members...")
+		fmt.Printf("Block HTML: %s\n", extendAResidenceTitleHTML)
+		err := toggleFamilyMembers(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to toggle family members: %w", err)
+		}
+
+		chromedp.Run(ctx,
+			chromedp.WaitVisible(extendAResidenceTitleXPath, chromedp.BySearch),
+			chromedp.OuterHTML(extendAResidenceTitleXPath, &extendAResidenceTitleHTML, chromedp.BySearch),
+		)
+	}
+
+	chromedp.Run(ctx,
 		chromedp.Click(extendAResidenceTitleXPath, chromedp.BySearch, chromedp.NodeVisible),
 	)
 
@@ -131,16 +138,18 @@ func initializeSearch(ctx context.Context) error {
 	for time.Since(t) < 20*time.Minute {
 		log.Println("Checking for error message...")
 
-		errors := []*cdp.Node{}
+		var html string
 		chromedp.Run(ctx,
-			chromedp.Nodes(`//*[@id="messagesBox"]/ul/li`, &errors, chromedp.BySearch),
+			// chromedp.Nodes(`//*[@id="messagesBox"]/ul/li`, &errors, chromedp.BySearch),
+			chromedp.OuterHTML("#main", &html, chromedp.ByQuery),
 		)
 
-		if len(errors) > 0 && errors[0].AttributeValue("class") == "errorMessage" {
-			fmt.Println("No dates found, retrying")
-		} else {
+		if strings.Contains(html, "Time Selection") {
+			fmt.Println(html)
 			fmt.Println("WTF FOUND AN APPOINTMENT")
 			break
+		} else {
+			fmt.Println("No dates found, retrying")
 		}
 
 		fmt.Println("Done.")
@@ -152,6 +161,62 @@ func initializeSearch(ctx context.Context) error {
 			chromedp.WaitVisible(searchAppointmentXPath, chromedp.BySearch),
 		)
 		fmt.Println("Done.")
+	}
+
+	return nil
+}
+
+func fillForm(ctx context.Context) error {
+	countrySelectorDropdownXPath := `//*[@id="xi-sel-400"]`
+	countries := []*cdp.Node{}
+
+	chromedp.Run(ctx,
+		chromedp.WaitVisible(countrySelectorDropdownXPath, chromedp.BySearch),
+		chromedp.Nodes(countrySelectorDropdownXPath, &countries, chromedp.BySearch),
+	)
+
+	for _, c := range countries {
+		for _, child := range c.Children {
+			if len(child.Attributes) < countryNameAttributeIdx {
+				fmt.Printf("%+v\n", child)
+				return fmt.Errorf("malformed country dropdown attributes")
+			}
+
+			if strings.Contains(child.Attributes[5], desiredCountry) {
+				err := chromedp.Run(ctx,
+					// this form is super janky idk why but it needs this
+					chromedp.Sleep(2*time.Second),
+					chromedp.SetValue(`//select[@id="xi-sel-400"]`, child.Attributes[3], chromedp.BySearch),
+
+					// this sets it to one person
+					chromedp.Sleep(time.Second),
+					chromedp.SetValue(`//select[@id="xi-sel-422"]`, "1", chromedp.BySearch),
+					// this sets it to no other family
+					chromedp.Sleep(time.Second),
+					chromedp.SetValue(`//select[@id="xi-sel-427"]`, "2", chromedp.BySearch),
+				)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func toggleFamilyMembers(ctx context.Context) error {
+	err := chromedp.Run(ctx,
+		// this sets it to yes family
+		chromedp.Sleep(time.Second),
+		chromedp.SetValue(`//select[@id="xi-sel-427"]`, "1", chromedp.BySearch),
+
+		// this sets it to no other family
+		chromedp.Sleep(time.Second),
+		chromedp.SetValue(`//select[@id="xi-sel-427"]`, "2", chromedp.BySearch),
+	)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 
 	return nil
